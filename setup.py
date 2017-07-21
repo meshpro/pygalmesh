679e177ecf35646a88ee253acdf9058a039bce34
Nico Schlömer <nico.schloemer@gmail.com>
2017-07-21 09:48:32 +0200

    adapt setup.py for pybind11

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
 #
+import os
+import sys
+
+import codecs
+import setuptools
 from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
-import os
-import codecs
+
 
 __version__ = '0.1.5'
 __author__ = u'Nico Schlömer'
@@ -13,28 +17,77 @@ __license__ = 'License :: OSI Approved :: MIT License'
 __url__ = 'https://github.com/nschloe/frentos'
 
 
+# https://github.com/pybind/python_example/blob/master/setup.py
 class BuildExt(build_ext):
-    # <http://stackoverflow.com/a/21236111/353337>
-    sub_commands = [
-        ('build_ext', build_ext.has_ext_modules),
-        ('build_py', build_ext.has_pure_modules),
-        ('build_clib', build_ext.has_c_libraries),
-        ('build_scripts', build_ext.has_scripts),
-        ]
+    '''A custom build extension for adding compiler-specific options.'''
+    c_opts = {
+        'msvc': ['/EHsc'],
+        'unix': [],
+        }
+
+    if sys.platform == 'darwin':
+        c_opts['unix'] += ['-stdlib=libc++', '-mmacosx-version-min=10.7']
+
+    def build_extensions(self):
+        ct = self.compiler.compiler_type
+        opts = self.c_opts.get(ct, [])
+        if ct == 'unix':
+            opts.append(
+                '-DVERSION_INFO=\'%s\'' % self.distribution.get_version()
+                )
+            opts.append(cpp_flag(self.compiler))
+            if has_flag(self.compiler, '-fvisibility=hidden'):
+                opts.append('-fvisibility=hidden')
+        elif ct == 'msvc':
+            opts.append(
+                '/DVERSION_INFO=\'%s\'' % self.distribution.get_version()
+                )
+        for ext in self.extensions:
+            ext.extra_compile_args = opts
+        build_ext.build_extensions(self)
 
 
-# class get_pybind_include(object):
-#     '''Helper class to determine the pybind11 include path
-#     The purpose of this class is to postpone importing pybind11
-#     until it is actually installed, so that the ``get_include()``
-#     method can be invoked.
-#     '''
-#     def __init__(self, user=False):
-#         self.user = user
-#
-#     def __str__(self):
-#         import pybind11
-#         return pybind11.get_include(self.user)
+class get_pybind_include(object):
+    '''Helper class to determine the pybind11 include path
+    The purpose of this class is to postpone importing pybind11
+    until it is actually installed, so that the ``get_include()``
+    method can be invoked.
+    '''
+    def __init__(self, user=False):
+        self.user = user
+
+    def __str__(self):
+        import pybind11
+        return pybind11.get_include(self.user)
+
+
+# As of Python 3.6, CCompiler has a `has_flag` method.
+# cf http://bugs.python.org/issue26689
+def has_flag(compiler, flagname):
+    '''Return a boolean indicating whether a flag name is supported on
+    the specified compiler.
+    '''
+    import tempfile
+    with tempfile.NamedTemporaryFile('w', suffix='.cpp') as f:
+        f.write('int main (int argc, char **argv) { return 0; }')
+        try:
+            compiler.compile([f.name], extra_postargs=[flagname])
+        except setuptools.distutils.errors.CompileError:
+            return False
+    return True
+
+
+def cpp_flag(compiler):
+    '''Return the -std=c++[11/14] compiler flag.
+    The c++14 is prefered over c++11 (when it is available).
+    '''
+    if has_flag(compiler, '-std=c++14'):
+        return '-std=c++14'
+    elif has_flag(compiler, '-std=c++11'):
+        return '-std=c++11'
+    else:
+        raise RuntimeError('Unsupported compiler -- at least C++11 support '
+                           'is needed!')
 
 
 def read(fname):
@@ -50,29 +103,30 @@ def read(fname):
     return content
 
 
-setup(
-    name='frentos',
-    cmdclass={'build_ext': BuildExt},
-    py_modules=['frentos'],
-    ext_modules=[Extension(
-        '_frentos',
+ext_modules = [Extension(
+        'frentos',
         [
             'src/generate.cpp',
             'src/generate_from_off.cpp',
             'src/generate_surface_mesh.cpp',
-            'src/frentos.i'
-        ],
+            'src/pybind11.cpp',
+            ],
         language='c++',
         include_dirs=[
             '/usr/include/eigen3/',
             # Path to pybind11 headers
-            # get_pybind_include(),
-            # get_pybind_include(user=True)
+            get_pybind_include(),
+            get_pybind_include(user=True)
             ],
         libraries=['CGAL', 'gmp', 'mpfr'],
-        swig_opts=['-keyword', '-c++', '-modern'],
-        extra_compile_args=['-std=c++11']
-        )],
+        # extra_compile_args=['-std=c++11']
+        )]
+
+setup(
+    name='frentos',
+    cmdclass={'build_ext': BuildExt},
+    py_modules=['frentos'],
+    ext_modules=ext_modules,
     package_dir={'': 'src'},
     version=__version__,
     url=__url__,
@@ -80,7 +134,8 @@ setup(
     author=__author__,
     author_email=__author_email__,
     install_requires=[
-        'numpy'
+        'numpy',
+        'pybind11>=1.7'
         ],
     description='3D mesh generation',
     long_description=read('README.rst'),
