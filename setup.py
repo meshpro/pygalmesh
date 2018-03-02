f170e8bfe6b290421ef67ae53fe2c133dc0dbd99
Nico Schlömer <nico.schloemer@gmail.com>
2018-03-02 12:01:20 +0100

    attempted fix for setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 #
 import os
-import sys
+# import sys
 
 import codecs
-import setuptools
+# import setuptools
 from setuptools import setup, Extension, find_packages
-from setuptools.command.build_ext import build_ext
+# from setuptools.command.build_ext import build_ext
 
 
 # https://packaging.python.org/single_source_version/
@@ -17,34 +17,34 @@ with open(os.path.join(base_dir, 'pygalmesh', '__about__.py'), 'rb') as handle:
     exec(handle.read(), about)
 
 
-# https://github.com/pybind/python_example/blob/master/setup.py
-class BuildExt(build_ext):
-    '''A custom build extension for adding compiler-specific options.'''
-    c_opts = {
-        'msvc': ['/EHsc'],
-        'unix': [],
-        }
-
-    if sys.platform == 'darwin':
-        c_opts['unix'] += ['-stdlib=libc++', '-mmacosx-version-min=10.7']
-
-    def build_extensions(self):
-        ct = self.compiler.compiler_type
-        opts = self.c_opts.get(ct, [])
-        if ct == 'unix':
-            opts.append(
-                '-DVERSION_INFO=\'%s\'' % self.distribution.get_version()
-                )
-            opts.append(cpp_flag(self.compiler))
-            if has_flag(self.compiler, '-fvisibility=hidden'):
-                opts.append('-fvisibility=hidden')
-        elif ct == 'msvc':
-            opts.append(
-                '/DVERSION_INFO=\'%s\'' % self.distribution.get_version()
-                )
-        for ext in self.extensions:
-            ext.extra_compile_args = opts
-        build_ext.build_extensions(self)
+# # https://github.com/pybind/python_example/blob/master/setup.py
+# class BuildExt(build_ext):
+#     '''A custom build extension for adding compiler-specific options.'''
+#     c_opts = {
+#         'msvc': ['/EHsc'],
+#         'unix': [],
+#         }
+#
+#     if sys.platform == 'darwin':
+#         c_opts['unix'] += ['-stdlib=libc++', '-mmacosx-version-min=10.7']
+#
+#     def build_extensions(self):
+#         ct = self.compiler.compiler_type
+#         opts = self.c_opts.get(ct, [])
+#         if ct == 'unix':
+#             opts.append(
+#                 '-DVERSION_INFO=\'%s\'' % self.distribution.get_version()
+#                 )
+#             opts.append(cpp_flag(self.compiler))
+#             if has_flag(self.compiler, '-fvisibility=hidden'):
+#                 opts.append('-fvisibility=hidden')
+#         elif ct == 'msvc':
+#             opts.append(
+#                 '/DVERSION_INFO=\'%s\'' % self.distribution.get_version()
+#                 )
+#         for ext in self.extensions:
+#             ext.extra_compile_args = opts
+#         build_ext.build_extensions(self)
 
 
 class get_pybind_include(object):
@@ -61,33 +61,33 @@ class get_pybind_include(object):
         return pybind11.get_include(self.user)
 
 
-# As of Python 3.6, CCompiler has a `has_flag` method.
-# cf http://bugs.python.org/issue26689
-def has_flag(compiler, flagname):
-    '''Return a boolean indicating whether a flag name is supported on
-    the specified compiler.
-    '''
-    import tempfile
-    with tempfile.NamedTemporaryFile('w', suffix='.cpp') as f:
-        f.write('int main (int argc, char **argv) { return 0; }')
-        try:
-            compiler.compile([f.name], extra_postargs=[flagname])
-        except setuptools.distutils.errors.CompileError:
-            return False
-    return True
-
-
-def cpp_flag(compiler):
-    '''Return the -std=c++[11/14] compiler flag.
-    The c++14 is preferred over c++11 (when it is available).
-    '''
-    if has_flag(compiler, '-std=c++14'):
-        return '-std=c++14'
-    elif has_flag(compiler, '-std=c++11'):
-        return '-std=c++11'
-    else:
-        raise RuntimeError('Unsupported compiler -- at least C++11 support '
-                           'is needed!')
+# # As of Python 3.6, CCompiler has a `has_flag` method.
+# # cf http://bugs.python.org/issue26689
+# def has_flag(compiler, flagname):
+#     '''Return a boolean indicating whether a flag name is supported on
+#     the specified compiler.
+#     '''
+#     import tempfile
+#     with tempfile.NamedTemporaryFile('w', suffix='.cpp') as f:
+#         f.write('int main (int argc, char **argv) { return 0; }')
+#         try:
+#             compiler.compile([f.name], extra_postargs=[flagname])
+#         except setuptools.distutils.errors.CompileError:
+#             return False
+#     return True
+#
+#
+# def cpp_flag(compiler):
+#     '''Return the -std=c++[11/14] compiler flag.
+#     The c++14 is preferred over c++11 (when it is available).
+#     '''
+#     if has_flag(compiler, '-std=c++14'):
+#         return '-std=c++14'
+#     elif has_flag(compiler, '-std=c++11'):
+#         return '-std=c++11'
+#     else:
+#         raise RuntimeError('Unsupported compiler -- at least C++11 support '
+#                            'is needed!')
 
 
 def read(fname):
@@ -125,7 +125,7 @@ ext_modules = [Extension(
 setup(
     name='pygalmesh',
     packages=find_packages(),
-    cmdclass={'build_ext': BuildExt},
+    # cmdclass={'build_ext': BuildExt},
     ext_modules=ext_modules,
     version=about['__version__'],
     url=about['__url__'],
