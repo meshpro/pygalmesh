4585f3734520b0f4f4452d144c081189f4990f71
Nico Schlömer <nico.schloemer@gmail.com>
2018-05-20 15:04:09 +0200

    version bump

--- pygalmesh/__about__.py
+++ pygalmesh/__about__.py
@@ -6,7 +6,7 @@ __author_email__ = 'nico.schloemer@gmail.com'
 __copyright__ = \
     u'Copyright (c) 2016-2018, {} <{}>'.format(__author__, __author_email__)
 __license__ = 'License :: OSI Approved :: MIT License'
-__version__ = '0.2.4'
+__version__ = '0.2.5'
 __maintainer__ = u'Nico Schlömer'
 __status__ = 'Development Status :: 4 - Beta'
 __url__ = 'https://github.com/nschloe/pygalmesh'
