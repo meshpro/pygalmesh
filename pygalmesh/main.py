3e4d4969ea6c5f0b616715e49aac54176e6edd13
Nico Schlömer <nico.schloemer@gmail.com>
2019-03-25 11:11:37 +0100

    add missing main.py

---
+++ pygalmesh/main.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+#
+import os
+import tempfile
+
+import meshio
+
+from _pygalmesh import _generate_mesh
+
+
+def generate_mesh(
+    domain,
+    feature_edges=None,
+    bounding_sphere_radius=0.0,
+    boundary_precision=1.0e-4,
+    lloyd=False,
+    odt=False,
+    perturb=True,
+    exude=True,
+    edge_size=0.0,
+    facet_angle=0.0,
+    facet_size=0.0,
+    facet_distance=0.0,
+    cell_radius_edge_ratio=0.0,
+    cell_size=0.0,
+    verbose=True,
+):
+    feature_edges = [] if feature_edges is None else feature_edges
+
+    outfile = tempfile.mkstemp(suffix=".mesh")
+
+    _generate_mesh(
+        domain,
+        outfile,
+        feature_edges=feature_edges,
+        bounding_sphere_radius=bounding_sphere_radius,
+        boundary_precision=boundary_precision,
+        lloyd=lloyd,
+        odt=odt,
+        perturb=perturb,
+        exude=exude,
+        edge_size=edge_size,
+        facet_angle=facet_angle,
+        facet_size=facet_size,
+        facet_distance=facet_distance,
+        cell_radius_edge_ratio=cell_radius_edge_ratio,
+        cell_size=cell_size,
+        verbose=verbose,
+    )
+
+    mesh = meshio.read(outfile)
+    os.remove(outfile)
+    return mesh
