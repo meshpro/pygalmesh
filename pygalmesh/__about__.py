3bb56c339fe0a2908f3f7ced6789a72fb065e02b
Nico Schlömer <nico.schloemer@gmail.com>
2020-02-19 12:55:40 +0100

    bump year

--- pygalmesh/__about__.py
+++ pygalmesh/__about__.py
@@ -1,6 +1,6 @@
 __author__ = "Nico Schlömer"
 __author_email__ = "nico.schloemer@gmail.com"
-__copyright__ = "Copyright (C) 2016-2019 {} <{}>".format(__author__, __author_email__)
+__copyright__ = "Copyright (C) 2016-2020 {} <{}>".format(__author__, __author_email__)
 __license__ = "License :: OSI Approved :: MIT License"
 __version__ = "0.5.0"
 __maintainer__ = "Nico Schlömer"
