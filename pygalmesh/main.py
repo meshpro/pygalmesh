cd8d16e4d291a52eb93caf56d7d7310c575d6b73
Umberto Villa <uvilla@wustl.edu>
2020-04-15 13:16:22 -0500

    Add a new functionality to specify cell size when meshing inr images. Add a inr writer to convert numpy array to inr mesh

--- pygalmesh/main.py
+++ pygalmesh/main.py
@@ -4,6 +4,7 @@ import tempfile
 import meshio
 from _pygalmesh import (
     _generate_from_inr,
+    _generate_from_inr_with_subdomain_sizing,
     _generate_from_off,
     _generate_mesh,
     _generate_periodic_mesh,
@@ -253,6 +254,52 @@ def generate_from_inr(
     os.remove(outfile)
     return mesh
 
+def generate_from_inr_with_subdomain_sizing(
+    inr_filename,
+    cell_sizes_map,
+    lloyd=False,
+    odt=False,
+    perturb=True,
+    exude=True,
+    edge_size=0.0,
+    facet_angle=0.0,
+    facet_size=0.0,
+    facet_distance=0.0,
+    cell_radius_edge_ratio=0.0,
+    verbose=True,
+):
+    fh, outfile = tempfile.mkstemp(suffix=".mesh")
+    os.close(fh)
+    
+    if "default" in cell_sizes_map.keys():
+        default_cell_size = cell_sizes_map.pop("default")
+    else:
+        default_cell_size = 0.
+    
+    cell_sizes  = [cell_sizes_map[k] for k in cell_sizes_map.keys() ]
+    subdomain_labels = list( cell_sizes_map.keys() )
+
+    _generate_from_inr_with_subdomain_sizing(
+        inr_filename,
+        outfile,
+        default_cell_size,
+        cell_sizes,
+        subdomain_labels,
+        lloyd=lloyd,
+        odt=odt,
+        perturb=perturb,
+        exude=exude,
+        edge_size=edge_size,
+        facet_angle=facet_angle,
+        facet_size=facet_size,
+        facet_distance=facet_distance,
+        cell_radius_edge_ratio=cell_radius_edge_ratio,
+        verbose=verbose,
+    )
+
+    mesh = meshio.read(outfile)
+    os.remove(outfile)
+    return mesh
 
 def remesh_surface(
     filename,
@@ -284,4 +331,91 @@ def remesh_surface(
     mesh = meshio.read(outfile)
     os.remove(off_file)
     os.remove(outfile)
+    return mesh
+
+def saveinr(vol, h, fname):
+    """
+    Save a volume (described as a numpy array) to INR format.
+    Code inspired by iso2mesh (http://iso2mesh.sf.net) by Q. Fang
+    INPUTS:
+    - vol: volume as numpy array
+    - h: voxel sizes as list or numpy array
+    - fname: filename for saving the inr file
+    """
+    
+    fid = open(fname, 'wb')
+    vol_type = vol.dtype
+    
+    if vol_type=='uint8':
+        btype='unsigned fixed'
+        bitlen=8
+    elif vol_type=='uint16':
+        btype='unsigned fixed'
+        bitlen=16
+    elif vol_type=='float32':
+        btype='float'
+        bitlen=32
+    elif vol_type=='float64':
+        btype='float'
+        bitlen=64
+    else:
+        raise(vol_type)
+    
+    header = ('#INRIMAGE-4#{8:s}\nXDIM={0:d}\nYDIM={1:d}\nZDIM={2:d}\nVDIM=1\nTYPE={3:s}\n' +
+              'PIXSIZE={4:d} bits\nCPU=decm\nVX={5:f}\nVY={6:f}\nVZ={7:f}\n').format(*vol.shape, btype, bitlen, h[0], h[1], h[2], '{')
+              
+    header = header+ '\n'*(256-4-len(header))+'##}\n'
+    
+    fid.write(header.encode('ascii'))
+    fid.write(vol.tobytes(order='F'))
+    
+def generate_from_array(
+    vol,
+    h,
+    lloyd=False,
+    odt=False,
+    perturb=True,
+    exude=True,
+    edge_size=0.0,
+    facet_angle=0.0,
+    facet_size=0.0,
+    facet_distance=0.0,
+    cell_radius_edge_ratio=0.0,
+    cell_size=0.0,
+    verbose=True,
+):
+    fh, inr_filename = tempfile.mkstemp(suffix=".inr")
+    os.close(fh)
+    saveinr(vol, h, inr_filename)
+    mesh = generate_from_inr(inr_filename, lloyd, odt, perturb, exude,
+                    edge_size, facet_angle, facet_size, facet_distance,
+                    cell_radius_edge_ratio, cell_size, verbose)
+    os.remove(inr_filename)
+    return mesh
+
+def generate_from_array_with_subdomain_sizing(
+    vol,
+    h,
+    cell_sizes_map,
+    lloyd=False,
+    odt=False,
+    perturb=True,
+    exude=True,
+    edge_size=0.0,
+    facet_angle=0.0,
+    facet_size=0.0,
+    facet_distance=0.0,
+    cell_radius_edge_ratio=0.0,
+    verbose=True,
+):
+    assert vol.dtype in ['uint8', 'uint16']
+    fh, inr_filename = tempfile.mkstemp(suffix=".inr")
+    os.close(fh)
+    saveinr(vol, h, inr_filename)
+    mesh = generate_from_inr_with_subdomain_sizing(
+                    inr_filename, cell_sizes_map,
+                    lloyd, odt, perturb, exude,
+                    edge_size, facet_angle, facet_size, facet_distance,
+                    cell_radius_edge_ratio, verbose)
+    os.remove(inr_filename)
     return mesh
--- src/generate_from_inr.hpp
+++ src/generate_from_inr.hpp
@@ -22,6 +22,25 @@ void generate_from_inr(
     const bool verbose = true
     );
 
+void
+generate_from_inr_with_subdomain_sizing(
+    const std::string & inr_filename,
+    const std::string & outfile,
+	const double default_cell_size,
+    const std::vector<double> & cell_sizes,
+	const std::vector<int> & cell_labels,
+    const bool lloyd = false,
+    const bool odt = false,
+    const bool perturb  = true,
+    const bool exude = true,
+    const double edge_size = 0.0,
+    const double facet_angle = 0.0,
+    const double facet_size = 0.0,
+    const double facet_distance = 0.0,
+    const double cell_radius_edge_ratio = 0.0,
+    const bool verbose  = true
+    );
+
 } // namespace pygalmesh
 
 #endif // GENERATE_FROM_INR_HPP
