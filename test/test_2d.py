9376f77fd39907557272e3a1e8a7092941948e76
Nico Schlömer <nico.schloemer@gmail.com>
2020-09-14 12:52:36 +0200

    add code for showing mesh

--- test/test_2d.py
+++ test/test_2d.py
@@ -12,6 +12,20 @@ def test_2d():
     assert points.shape == (276, 2)
     assert cells.shape == (486, 3)
 
+    # show mesh
+    # import matplotlib.pyplot as plt
+    # pts = points[cells]
+    # for pt in pts:
+    #     plt.plot([pt[0][0], pt[1][0]], [pt[0][1], pt[1][1]], "-k")
+    #     plt.plot([pt[1][0], pt[2][0]], [pt[1][1], pt[2][1]], "-k")
+    #     plt.plot([pt[2][0], pt[0][0]], [pt[2][1], pt[0][1]], "-k")
+    # # for pt in points:
+    # #     plt.plot(pt[0], pt[1], "or")
+    # plt.gca().set_aspect("equal")
+    # plt.show()
+
+
+
 
 if __name__ == "__main__":
     test_2d()
